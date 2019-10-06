7759fedfb141467751bb6ed9bccd880e365cc131
Penuel Maypa <penmayp@gmail.com>
2019-10-06 06:31:40 +0100

    Still in progress :  The black pawn can now move but it still needs to update the validate move

--- ChessProject/ChessProject.java
+++ ChessProject/ChessProject.java
@@ -218,6 +218,82 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 			demonstration purposes the Pawn here turns into a Queen.
 		*/
 
+		if(pieceName.equals("BlackPawn")){  // #solution_2
+			if(startY == 1)
+			{
+				if((startX == (e.getX()/75))&&((((e.getY()/75)-startY)==1)||((e.getY()/75)-startY)==2))
+				{
+					if((((e.getY()/75)-startY)==2)){
+						if((!piecePresent(e.getX(), (e.getY())))&&(!piecePresent(e.getX(), (e.getY()+75)))){
+							validMove = true;					
+						}
+						else{
+							validMove = false;
+						}							
+					}
+					else{
+						if((!piecePresent(e.getX(), (e.getY()))))
+						{
+							validMove = true;					
+						}	
+						else{
+							validMove = false;
+						}													
+					}
+				}
+				else{
+					validMove = false;					
+				}
+			}
+			else{
+				int newY = e.getY()/75;
+				int newX = e.getX()/75;				
+				if((startX-1 >=0)||(startX +1 <=7))
+				{
+					if((piecePresent(e.getX(), (e.getY())))&&((((newX == (startX+1)&&(startX+1<=7)))||((newX == (startX-1))&&(startX-1 >=0)))))
+					{
+						if(checkWhiteOponent(e.getX(), e.getY())){
+							
+							//Penuel #solution_1
+							if(yMovement >=2){
+								validMove = false;
+							}else{
+								validMove = true; //from : 
+							}
+
+							if(startY == 6){
+								success = true;
+							}						
+						}
+						else{
+							validMove = false;
+						}
+					}
+					else{
+						if(!piecePresent(e.getX(), (e.getY()))){
+							if((startX == (e.getX()/75))&&((e.getY()/75)-startY)==1){
+								if(startY == 6){
+									success = true;
+								}
+								validMove = true;
+							}
+							else{
+								validMove = false;
+							}				
+						}
+						else{
+							validMove = false;	
+						}
+					}
+				}
+				else{
+					validMove = false;
+				}				
+			}			
+		}
+		//=== Method End : BlackPawn
+
+
 		if(pieceName.equals("WhitePawn")){
 			if(startY == 1)
 			{
