82e207e5e69cba6a8493f1a67d3df1ccbc4824d9
Penuel Maypa <penmayp@gmail.com>
2019-10-07 21:27:14 +0100

    manually fixed merge conflict and overriding

--- ChessProject/ChessProject.java
+++ ChessProject/ChessProject.java
@@ -277,20 +277,7 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 					}
 				}
 				else{
-					if(checkBlackOponent(e.getX(), e.getY())){
-							
-							//Penuel #solution_1
-							if(yMovement >=2){
-								validMove = false;
-							}else{
-								validMove = true; //from : 
-							}
-
-							if(startY == 6){
-								success = true;
-							}						
-					}
-					// fronm :  validMove = false;					
+					validMove = false;					
 				}
 			}
 			// If it moves more 2 steps or move
@@ -392,7 +379,6 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 				int newX = e.getX()/75;				
 				if((startX-1 >=0)||(startX +1 <=7))
 				{
-					//starts from here !!!
 					if((piecePresent(e.getX(), (e.getY())))&&((((newX == (startX+1)&&(startX+1<=7)))||((newX == (startX-1))&&(startX-1 >=0)))))
 					{
 						if(checkWhiteOponent(e.getX(), e.getY())){
@@ -432,55 +418,53 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 				else{
 					validMove = false;
 				}				
-			}
-			
-			if(!validMove){		
-				int location=0;
-				if(startY ==0){
-					location = startX;
-				}
-				else{
-					location  = (startY*8)+startX;
-				}
-				String pieceLocation = pieceName+".png"; 
-				pieces = new JLabel( new ImageIcon(pieceLocation) );
-				panels = (JPanel)chessBoard.getComponent(location);
-				panels.add(pieces);			
-			}
-			else{
-				if(success){
-					int location = 56 + (e.getX()/75);
-					if (c instanceof JLabel){
-						Container parent = c.getParent();
-						parent.remove(0);
-						pieces = new JLabel( new ImageIcon("WhiteQueen.png") );
-						parent = (JPanel)chessBoard.getComponent(location);
-						parent.add(pieces);			
-					}
-					else{
-						Container parent = (Container)c;
-						pieces = new JLabel( new ImageIcon("WhiteQueen.png") );
-						parent = (JPanel)chessBoard.getComponent(location);
-						parent.add(pieces);	            	
-					}
-				}
-				else{
-					if (c instanceof JLabel){
-						Container parent = c.getParent();
-						parent.remove(0);
-						parent.add( chessPiece );
-					}
-					else {
-						Container parent = (Container)c;
-						parent.add( chessPiece );
-					}
-					chessPiece.setVisible(true);									
-				}
-			}
+			}			
 		}
 		//=== Method End : WhitePawn
 		
-		
+		if(!validMove){		
+			int location=0;
+			if(startY ==0){
+				location = startX;
+			}
+			else{
+				location  = (startY*8)+startX;
+			}
+			String pieceLocation = pieceName+".png"; 
+			pieces = new JLabel( new ImageIcon(pieceLocation) );
+			panels = (JPanel)chessBoard.getComponent(location);
+		    panels.add(pieces);			
+		}
+		else{
+			if(success){
+				int location = 56 + (e.getX()/75);
+				if (c instanceof JLabel){
+	            	Container parent = c.getParent();
+	            	parent.remove(0);
+					pieces = new JLabel( new ImageIcon("WhiteQueen.png") );
+					parent = (JPanel)chessBoard.getComponent(location);
+			    	parent.add(pieces);			
+				}
+				else{
+					Container parent = (Container)c;
+	            	pieces = new JLabel( new ImageIcon("WhiteQueen.png") );
+					parent = (JPanel)chessBoard.getComponent(location);
+			    	parent.add(pieces);	            	
+				}
+			}
+			else{
+				if (c instanceof JLabel){
+	            	Container parent = c.getParent();
+	            	parent.remove(0);
+	            	parent.add( chessPiece );
+	        	}
+	        	else {
+	            	Container parent = (Container)c;
+	            	parent.add( chessPiece );
+	        	}
+	    		chessPiece.setVisible(true);									
+			}
+		}
 
 			prevPositionY = e.getY();
 
