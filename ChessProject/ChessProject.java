5e62c38bb151598847bbd00ede84307a80eae128
Penuel Maypa <penmayp@gmail.com>
2019-10-10 02:15:47 +0100

    Problem found and fixed: Bishop is wrong speling, the spelling was bishup instead of bishop.

--- ChessProject/ChessProject.java
+++ ChessProject/ChessProject.java
@@ -252,7 +252,68 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 			demonstration purposes the Pawn here turns into a Queen.
 		*/
 
+		if(pieceName.contains("Bishop")){
+			Boolean inTheWay = false ;
+			int distance = Math.abs(startX - landingX);
 
+			if (((landingX < 0) || (landingX > 7)) || ((landingY < 0) || (landingY > 7 ))){
+				validMove = false;
+			}else{
+				validMove = true ;
+				if(Math.abs(startX - landingX) == Math.abs(startY - landingY)){
+					if((startX - landingX < 0) && (startY - landingY < 0 )){
+						for(int i = 0; i < distance ; i++){
+							if(piecePresent((initialX +(i*75)), (initialY +(i*75)))){
+								inTheWay = true ;
+							}
+						}
+					}
+				}
+			}
+
+			if(( startX - landingX < 0)&& (startY - landingY > 0)){
+				for (int i = 0; i < distance ; i++){
+					if(piecePresent((initialX + (i * 75)), (initialY - (i*75)))){
+						inTheWay = true ;
+					}
+				}
+			}
+			if((startX - landingX > 0) && (startY - landingY > 0)){
+				for(int i=0 ; i < distance ; i++){
+					if(piecePresent((initialX - (i*75)), (initialY - (i*75)))){
+						inTheWay = true ;
+					}
+				}
+			}
+
+			if(inTheWay){
+				validMove = false;
+			}
+			else{
+				if(piecePresent(e.getX(), (e.getY()))){
+					if(pieceName.contains("White")){
+						if(checkWhiteOponent(e.getX(), e.getY())){
+							validMove = true ;
+						}else{
+							validMove = false;
+						}
+					}
+					else{
+						if(checkBlackOponent(e.getX(), e.getY())){
+							validMove = true ;
+						}else{
+							validMove = false ;
+						}
+
+					}
+				}
+				else{
+					validMove = true ;
+				}
+			
+			}
+
+		}
 
 		if(pieceName.contains("Knight")){
 			if(((landingX < 0 ) || (landingX > 7)) || ((landingY < 0) || landingY > 7)){
@@ -325,7 +386,7 @@ public class ChessProject extends JFrame implements MouseListener, MouseMotionLi
 			else{ 
 				int newY = e.getY()/75;
 				int newX = e.getX()/75;				
-				 if (xMovement==0 && yMovement == 1 && isForwardMove == true)	// FROM :  if((startX-1 >=0)||(startX +1 <=7))
+				if (xMovement==0 && yMovement == 1 && isForwardMove == true)	// FROM :  if((startX-1 >=0)||(startX +1 <=7))
 				{
 					if((piecePresent(e.getX(), (e.getY())))&&((((newX == (startX+1)&&(startX+1<=7)))||((newX == (startX-1))&&(startX-1 >=0)))))
 					{
